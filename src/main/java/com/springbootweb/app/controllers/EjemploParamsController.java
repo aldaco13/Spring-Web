13f8ed06966ad69ca685777fcbc6c8d63ff14490
aldaco13 <omar.aldaco.m@gmail.com>
2023-08-05 22:28:55 -0500

    Se agrega ejemplo para recibir paràmetro por URL con thyeleaf

--- src/main/java/com/springbootweb/app/controllers/EjemploParamsController.java
+++ src/main/java/com/springbootweb/app/controllers/EjemploParamsController.java
@@ -3,17 +3,17 @@ package com.springbootweb.app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-/*
- * Ejemplo para mandar parámetros por URL
- * */
 
 @Controller
 @RequestMapping("/params")
 public class EjemploParamsController {
 
+	/*
+	 * Ejemplo para mandar parámetros por URL
+	 * */
 	@GetMapping("/string")
 	public String param(@RequestParam(required=false, defaultValue = "No se recibieron datos") String texto, Model model) {
 		
@@ -22,6 +22,9 @@ public class EjemploParamsController {
 		return "params/ver";
 	}
 	
+	/*
+	 * Ejemplo para recibir parámetros por URL
+	 * */
 	@GetMapping("/")
 	public String index() {
 		return "params/index";
