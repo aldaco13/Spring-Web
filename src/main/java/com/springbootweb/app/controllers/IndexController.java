f4dd554edee291cd4368f760557c97202254f427
aldaco13 <omar.aldaco.m@gmail.com>
2023-08-05 20:30:58 -0500

    Se agregan vista usuario y listar con lògica para mostrar la info dentro

--- src/main/java/com/springbootweb/app/controllers/IndexController.java
+++ src/main/java/com/springbootweb/app/controllers/IndexController.java
@@ -1,15 +1,21 @@
 package com.springbootweb.app.controllers;
 
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.springbootweb.app.models.Usuario;
 
 @Controller
 public class IndexController {
 	
 	public static final String INDEX = "index";
+	public static final String TRUE = "true";
+	
 	
 	/*@GetMapping({"/index", "/", "/home"})
 	public String index() {
@@ -17,13 +23,53 @@ public class IndexController {
 		return INDEX;
 	}*/
 	
-	@GetMapping("/index")
+	@GetMapping({"/index", "/", "/home"})
 	public String indexModel(Model model) {
 		System.out.println("entra indexModel");
+		model.addAttribute("muestraDesc", true);
 		model.addAttribute("descModel", "Texto insertado con thymeleaf desde el metodo index usando \"model.addAttribute\"");
 		model.addAttribute("tituloModel", "Hola Spring Framework! indexModel");
 		return INDEX;
 		
+	}
+	
+	@GetMapping("/rutaAux")
+	public String indexAux(Model model) {
+		model.addAttribute("muestraAux", true);
+		System.out.println("entra indexAux");
+		return INDEX;
+		
+	}
+	
+	@RequestMapping("/perfil")
+	public String perfil(Model model) {
+		Usuario user = new Usuario();
+		user.setNombre("Omar");
+		user.setApellido("Aldaco");
+		user.setGenero("H");
+		model.addAttribute("usuario", user);
+		return "perfil";
+	}
+	
+	@RequestMapping("/listar")
+	public String listar(Model model) {
+		List<Usuario> usuarios = new ArrayList<Usuario>();
+		
+		Usuario u2 = new Usuario();
+		u2.setNombre("Diego");
+		u2.setApellido("Aldaco");
+		
+		Usuario u3 = new Usuario();
+		u3.setNombre("Virginia");
+		u3.setApellido("Montalvo");
+		
+		usuarios.add(new Usuario("Omar", "Aldaco", "H"));
+		usuarios.add(u2);
+		usuarios.add(u3);
+		
+		model.addAttribute("titulo", "Listado Usuario");
+		model.addAttribute("usuarios", usuarios);
+		return "listar";
 	}
 		
 }
--- src/main/java/com/springbootweb/app/controllers/MapController.java
+++ src/main/java/com/springbootweb/app/controllers/MapController.java
@@ -4,15 +4,19 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/app")
 public class MapController {
 	
 	@GetMapping("/map")
-	public void pageMap(Map<String, Object> map) {
+	public String pageMap(Map<String, Object> map) {
 		System.out.println("entra pageMap");
 		map.put("descMap", "Texto insertado con thymeleaf desde el metodo indexMap usando \"Map<String, Object>\"");
 		map.put("tituloMap", "Hola Spring Framework! pageMap");		
+		
+		return "map";
 	}
 
 }
