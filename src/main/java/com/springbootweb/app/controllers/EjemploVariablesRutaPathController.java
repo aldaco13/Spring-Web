6aff4dabaceaff6fb55ab9a6c2eb86caf1481828
aldaco13 <omar.aldaco.m@gmail.com>
2023-08-06 01:04:47 -0500

    Se agregan ejemplos para 2 variables con PathVariable

--- src/main/java/com/springbootweb/app/controllers/EjemploVariablesRutaPathController.java
+++ src/main/java/com/springbootweb/app/controllers/EjemploVariablesRutaPathController.java
@@ -10,10 +10,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/variables")
 public class EjemploVariablesRutaPathController {
 	
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("titulo", "Enviar parámetros de la ruta (@PathVariable)");
+		return "variables/index";
+	}
+	
+	/*
+	 * Ejemplo para pasar un parámetro por URL
+	 */
+	
 	@GetMapping("/string/{texto}")
 	public String variables(@PathVariable String texto, Model model) {
 		model.addAttribute("titulo", "Recibir parámetros de la ruta (@PathVariable)");
 		model.addAttribute("resultado", "El texto enviado es: " + texto);
+		return "variables/ver";
+	}
+	
+	/*
+	 * Ejemplo para pasar dos parámetros por URL
+	 */
+	
+	@GetMapping("/string/{texto}/{numero}")
+	public String variables(@PathVariable String texto, @PathVariable Integer numero, Model model) {
+		model.addAttribute("titulo", "Enviar parámetros de la ruta (@PathVariable)");
+		model.addAttribute("resultado", "El texto enviado es: " + texto + " el número recibido es: " + numero);
 		return "variables/ver";
 	}
 
